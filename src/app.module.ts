a882567ba0b272a886fe9b2f679cce64b77ca28e
Samrat Bhadra <68498583+sammy368@users.noreply.github.com>
2026-01-04 12:01:32 +0530

    Database Connectivity Introduced Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DB_URL'),
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: ['error', 'info', 'warn'],
+        ssl: config.get('DB_URL')?.includes('ssl=true') ? { rejectUnauthorized: false } : false,
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+  constructor(private readonly dataSource: DataSource) {}
+  async onModuleInit() {
+    try {
+      await this.dataSource.query('SELECT 1');
+      this.logger.log('Database connection successful');
+    } catch (error) {
+      this.logger.log('Database connection failed', error);
+      process.exit(1);
+    }
+  }
+}
